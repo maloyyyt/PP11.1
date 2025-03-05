26bb5a3afa7cf40bfb00927445d540c6f39351f0
Артём Петров <roman1artem2haha@gmail.com>
2025-03-05 23:21:16 +0300

    init commit

---
+++ src/widget.py
@@ -0,0 +1,33 @@
+from src.masks import get_mask_card_number,get_mask_account
+
+
+def mask_account_card(card_info):
+    """
+    Принимает на вход строку формата "Visa Platinum 7000792289606361" и "Счет 73654108430135874305"
+    """
+    if "Счет" in card_info:
+        return get_mask_account(card_info)
+    else:
+        return get_mask_card_number(card_info)
+
+
+def get_date(date_str):
+    """
+    Принимает на вход в формате "2024-03-11T02:26:18.671407" возвращает в формате "ДД.ММ.ГГГГ"
+    """
+    try:
+        date_part = date_str.split('T')[0]
+        year, month, day = date_part.split('-')
+        return f"{day}.{month}.{year}"
+    except ValueError:
+        raise ValueError("Неверный формат даты")
+
+
+if __name__ == "__main__":
+    card_info = input("enter card number:")
+    account_info = input("enter account number:")
+    date_str = input("enter date:")
+
+    print(mask_account_card(card_info))
+    print(mask_account_card(account_info))
+    print(get_date(date_str))
